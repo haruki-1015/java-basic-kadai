30d4ab8b7246ab985702944ee388b9fac83f1b54
haruki-1015 <ryxstedp.nube@gmail.com>
2026-05-13 00:40:41 +0900

    Add files via upload

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+   
+	public  KatoIchiro_Chapter18() {
+        familyName = "加藤";
+        address = "東京都中野区〇×";
+        setGivenName();
+    }
+
+    public void setGivenName() {
+        this.givenName = "一郎";
+    }
+
+    public void eachIntroduce() {
+        System.out.println("好きな食べ物はリンゴです");
+    }
+}
